d7b73096bc330a4a56fd28f6b666923de84c6834
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-02-25 01:38:05 +0900

    setup default route

--- mavel/src/index.tsx
+++ mavel/src/index.tsx
@@ -1,8 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { reset } from "styled-reset";
+import { createGlobalStyle } from "styled-components";
+import { ChakraProvider } from "@chakra-ui/react";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  body {
+      background-color: #121212;
+      color: whitesmoke;
+      font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+  
+  *{
+      box-sizing: border-box;
+      text-decoration: none;
+  }
+`;
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+    <>
+        <GlobalStyle />
+        <ChakraProvider>
+            <App />
+        </ChakraProvider>
+    </>
+);
