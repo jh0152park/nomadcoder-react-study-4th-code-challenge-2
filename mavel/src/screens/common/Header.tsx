75e2ec4edadab2271f825c1ca06ce3c79bfed8fc
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-02-25 01:53:57 +0900

    added error page

---
+++ mavel/src/screens/common/Header.tsx
@@ -0,0 +1,6 @@
+export default function Header() {
+    const logo =
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/220px-Marvel_Logo.svg.png";
+
+    return null;
+}
