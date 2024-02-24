bdcc7822c9c1001ead9e773c175c7407552b735d
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-02-25 02:40:20 +0900

    added interfaces

---
+++ mavel/src/types.ts
@@ -0,0 +1,79 @@
+interface IThumbnail {
+    path: string;
+    extension: string;
+}
+
+interface IComicItem {
+    resourceURI: string;
+    name: string;
+}
+
+interface ISeriesItem {
+    resourceURI: string;
+    name: string;
+}
+
+interface IComics {
+    available: number;
+    collectionURI: string;
+    items: IComicItem[];
+    returned: number;
+}
+
+interface ISeries {
+    available: number;
+    collectionURI: string;
+    items: ISeriesItem[];
+    returned: number;
+}
+
+interface IStoryItem {
+    resourceURI: string;
+    name: string;
+    type: string;
+}
+
+interface IStories {
+    available: number;
+    collectionURI: string;
+    items: IStoryItem[];
+    returned: number;
+}
+
+interface IEventItem {
+    resourceURI: string;
+    name: string;
+}
+
+interface IEvents {
+    available: number;
+    collectionURI: string;
+    items: IEventItem[];
+    returned: number;
+}
+
+interface IUrls {
+    type: string;
+    url: string;
+}
+
+interface IEntireCharacter {
+    id: number;
+    name: string;
+    description: string;
+    thumbnail: IThumbnail;
+    resourceURI: string;
+    comics: IComics;
+    series: ISeries;
+    stories: IStories;
+    events: IEvents;
+    urls: IUrls[];
+}
+
+export interface IEntireCharacterResponse {
+    data: {
+        data: {
+            results: IEntireCharacter[];
+        };
+    };
+}
